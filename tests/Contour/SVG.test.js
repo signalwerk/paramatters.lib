bce6e3f64e69bd87bc52e22f2f8bd86c530dd7e9
Stefan Huber <sh@signalwerk.ch>
2019-05-08 19:34:26 +0200

    ADD: test stub

---
+++ tests/Contour/SVG.test.js
@@ -0,0 +1,15 @@
+
+
+
+// W3C
+// Superfluous white space and separators (such as commas) may be eliminated
+// M 100, -100 L 200, 200 === M 100 -100 L 200 200 === M100 -100L200 200 === M100-100L200,200
+
+
+// W3C
+// A command letter may be eliminated if an identical command letter would otherwise precede it;
+// M 100 200 L 200 100 L -100 -200
+
+
+
+// "M0,0 10,0 10,10 0,10z" ===
